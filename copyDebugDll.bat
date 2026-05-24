8cef2a15cb11c75bd5dd915cc2be4e9c7f725f84
disro <disro@outlook.com>
2026-05-25 00:02:59 +0700

    Refactoring

--- copyDebugDll.bat
+++
@@ -1,4 +0,0 @@
-echo $TargetDir
-echo $ProjectName
-copy /Y "$(ProjectDir)${OutDir}$(ProjectName).dll" "C:\Users\disro\AppData\Roaming\r2modmanPlus-local\RiskOfRain2\profiles\ListEclipseLevelsMod\BepInEx\plugins\$(ProjectName).dll"
-copy /Y "$(ProjectDir)${OutDir}$(ProjectName).pdb" "C:\Users\disro\AppData\Roaming\r2modmanPlus-local\RiskOfRain2\profiles\ListEclipseLevelsMod\BepInEx\plugins\$(ProjectName).pdb"
